6b6cd44783730097b52ce5309f4f053c916882bb
Singh <akash.m.singh@capgemini.com>
2019-05-07 19:34:58 +0530

    Load Balancing added

---
+++ spring-boot-microservice-currency-conversion/src/main/java/com/capgemini/springbootmicroservicecurrencyconversion/dao/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,15 @@
+package com.capgemini.springbootmicroservicecurrencyconversion.dao;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.capgemini.springbootmicroservicecurrencyconversion.bean.CurrencyConversionBean;
+
+
+@FeignClient(name="forex-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  public CurrencyConversionBean retrieveExchangeValue
+    (@PathVariable("from") String from, @PathVariable("to") String to);
+}
